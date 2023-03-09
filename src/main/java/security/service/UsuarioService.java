db4f4c560616002a47a5fac9d32563d431d5b15e
caiocfpeixoto <63310044+caiocfpeixoto@users.noreply.github.com>
2023-03-08 23:46:36 -0300

    Preparando acesso por usuario

---
+++ src/main/java/security/service/UsuarioService.java
@@ -0,0 +1,50 @@
+package security.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import security.domain.Perfil;
+import security.domain.Usuario;
+import security.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService implements UserDetailsService{
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	
+	@Transactional(readOnly = true)
+	public Usuario buscarPorEmail(String email) {
+		
+		return repository.findByEmail(email);
+	}
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = buscarPorEmail(username);
+		return new User(
+				usuario.getEmail(),
+				usuario.getSenha(),
+				AuthorityUtils.createAuthorityList(getAuthorities(usuario.getPerfis())));
+	}
+	
+	private String[] getAuthorities(List<Perfil> perfis) {
+		String[] authorities = new String[perfis.size()];
+		for (int i = 0; i < perfis.size(); i++) {
+			authorities[i] = perfis.get(i).getDesc();
+		}
+		return authorities;
+	}
+	
+	
+}
